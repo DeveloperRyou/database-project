c85e559ae6feb848b38f962fd7386c4c304bdcab
DeveloperRyou <DeveloperRyou@gmail.com>
2023-11-29 19:27:26 +0900

    FIX: 메인화면 디자인 변경

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,48 +1,13 @@
-import Container from './container'
-import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Container from "./container";
 
-type Props = {
-  preview?: boolean
-}
-
-const Alert = ({ preview }: Props) => {
+const Alert = () => {
   return (
-    <div
-      className={cn('border-b', {
-        'bg-neutral-800 border-neutral-800 text-white': preview,
-        'bg-neutral-50 border-neutral-200': !preview,
-      })}
-    >
+    <div className="border-b fixed top-0 w-full bg-neutral-200 border-neutral-300">
       <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This page is a preview.{' '}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-teal-300 duration-200 transition-colors"
-              >
-                Click here
-              </a>{' '}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-blue-600 duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
-        </div>
+        <div className="py-2 text-center text-sm">감쇄율</div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Alert
+export default Alert;
