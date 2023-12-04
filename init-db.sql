94fd057b0f2a422b3e20bdca164f2c4f6d1beb22
DeveloperRyou <DeveloperRyou@gmail.com>
2023-12-05 00:18:26 +0900

    FEAT: 테이블 sql 생성

--- init-db.sql
+++ init-db.sql
@@ -22,3 +22,57 @@ INSERT INTO `users` (`user_id`, `email_id`, `password`, `name`, `type`, `birth`,
 	(4, 'user3', '1234', 'user계정3', 0, NULL, NULL, NULL, NULL),
 	(5, 'user4', '1234', 'user계정4', 0, NULL, NULL, NULL, NULL),
 	(6, 'user5', '1234', 'user계정5', 0, NULL, NULL, NULL, NULL);
+
+CREATE TABLE IF NOT EXISTS `importance` (
+  `importance_id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` double NOT NULL,
+  PRIMARY KEY (`importance_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `article` (
+  `article_id` int(11) NOT NULL AUTO_INCREMENT,
+  `writer_id` int(11) NOT NULL,
+  `importance_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `like_count` int(11) NOT NULL,
+  `view_count` int(11) NOT NULL,
+  `create_at` datetime NOT NULL,
+  `update_at` datetime NOT NULL,
+  CONSTRAINT `fk_article_writer` FOREIGN KEY (`writer_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT,
+  CONSTRAINT `fk_article_importance` FOREIGN KEY (`importance_id`) REFERENCES `importance` (`importance_id`) ON DELETE RESTRICT,
+  PRIMARY KEY (`article_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `writer_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `importance_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `like_count` int(11) NOT NULL,
+  `view_count` int(11) NOT NULL,
+  `create_at` datetime NOT NULL,
+  `update_at` datetime NOT NULL,
+  CONSTRAINT `fk_comment_writer` FOREIGN KEY (`writer_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT,
+  CONSTRAINT `fk_comment_article` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE RESTRICT,
+  CONSTRAINT `fk_comment_importance` FOREIGN KEY (`importance_id`) REFERENCES `importance` (`importance_id`) ON DELETE RESTRICT,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `like_article_relation` (
+  `like_article_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  CONSTRAINT `fk_like_article_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT,
+  CONSTRAINT `fk_like_article_article` FOREIGN KEY (`article_id`) REFERENCES `article` (`article_id`) ON DELETE RESTRICT,
+  PRIMARY KEY (`like_article_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `like_comment_relation` (
+  `like_comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  CONSTRAINT `fk_like_comment_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT,
+  CONSTRAINT `fk_like_comment_comment` FOREIGN KEY (`comment_id`) REFERENCES `comment` (`comment_id`) ON DELETE RESTRICT,
+  PRIMARY KEY (`like_comment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
