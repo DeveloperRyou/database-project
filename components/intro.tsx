055156d9a254a1dd6e631d541e4101228b60130d
DeveloperRyou <DeveloperRyou@gmail.com>
2023-12-06 17:08:41 +0900

    FIX: 디자인 수정

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,10 +1,8 @@
 const Intro = () => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
-        류성민의 database project
-      </h1>
-    </section>
+    <h1 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mt-24 mb-16">
+      류성민의 database project
+    </h1>
   );
 };
 
