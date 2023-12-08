1c9494e35e692043f144637829a34614f9db7a26
DeveloperRyou <DeveloperRyou@gmail.com>
2023-12-08 22:35:19 +0900

    FEAT: 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout";
 import Container from "@/components/layout/container";
+import Header from "@/components/layout/header";
 import Head from "next/head";
 
 export default function Index() {
@@ -10,7 +11,8 @@ export default function Index() {
           <title>류성민의 database project</title>
         </Head>
         <Container>
-          <div className="pt-48 text-center">
+          <Header />
+          <div className="pt-36 text-center">
             <h1 className="text-9xl">404</h1>
             <h2 className="mt-8 text-4xl">Not Found</h2>
           </div>
