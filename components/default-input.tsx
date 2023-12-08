03cf8e3450d2d0b1e44ec6f41795c3304e06a216
DeveloperRyou <DeveloperRyou@gmail.com>
2023-12-08 22:15:39 +0900

    FIX: 검색, 정렬 기능 추가

--- components/default-input.tsx
+++ components/default-input.tsx
@@ -1,12 +1,15 @@
 export default function DefaultInput(
   props: React.InputHTMLAttributes<HTMLInputElement>
 ) {
+  const { className } = props;
+  const rest = { ...props };
+  delete rest.className;
   return (
     <input
       className={`w-full h-full px-4 py-2 border border-gray-600 ${
         props.disabled ? "bg-gray-200" : ""
-      }`}
-      {...props}
+      } ${className}`}
+      {...rest}
     />
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,62 @@ import Intro from "@/components/intro";
 import Layout from "@/components/layout";
 import MoreStories from "@/components/article/more-stories";
 import { Article, getAllArticles } from "@/lib/api/article";
+import DefaultInput from "@/components/default-input";
+import { useEffect, useState } from "react";
 
 type Props = {
   allArticles: Article[];
 };
 
 export default function Index({ allArticles }: Props) {
+  const [articles, setArticles] = useState<Article[]>(allArticles);
+  const [filter, setFilter] = useState<
+    "latest" | "view" | "like" | "importance"
+  >("latest");
+  const [search, setSearch] = useState<string>("");
+
+  useEffect(() => {
+    if (filter === "latest") {
+      setArticles(
+        allArticles.sort((a, b) => {
+          return (
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+          );
+        })
+      );
+    } else if (filter === "view") {
+      setArticles(
+        allArticles.sort((a, b) => {
+          return b.view_count - a.view_count;
+        })
+      );
+    } else if (filter === "like") {
+      setArticles(
+        allArticles.sort((a, b) => {
+          return b.like_count - a.like_count;
+        })
+      );
+    } else if (filter === "importance") {
+      setArticles(
+        allArticles.sort((a, b) => {
+          return b.importance_value - a.importance_value;
+        })
+      );
+    }
+  }, [filter]);
+
+  useEffect(() => {
+    if (search === "") {
+      setArticles(allArticles);
+    } else {
+      setArticles(
+        allArticles.filter((article) => {
+          return article.content.includes(search);
+        })
+      );
+    }
+  }, [search]);
+
   return (
     <>
       <Layout>
@@ -21,7 +71,47 @@ export default function Index({ allArticles }: Props) {
           <h3 className="mb-8 text-3xl md:text-5xl font-bold tracking-tighter leading-tight">
             게시글
           </h3>
-          {allArticles.length > 0 && <MoreStories articles={allArticles} />}
+          {allArticles.length > 0 && <MoreStories articles={articles} />}
+          <div className="flex justify-between">
+            <div className="flex gap-2">
+              <div className="h-fit my-auto">검색</div>
+              <DefaultInput
+                className=" max-w-[200px]"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </div>
+            <div className="flex gap-2">
+              <button
+                className={`border border-gray-600 px-2 py-1 hover:bg-gray-600 hover:text-white
+                 ${filter === "latest" ? "bg-gray-600 text-white" : ""}`}
+                onClick={() => setFilter("latest")}
+              >
+                최신순
+              </button>
+              <button
+                className={`border border-gray-600 px-2 py-1 hover:bg-gray-600 hover:text-white 
+                ${filter === "view" ? "bg-gray-600 text-white" : ""}`}
+                onClick={() => setFilter("view")}
+              >
+                조회순
+              </button>
+              <button
+                className={`border border-gray-600 px-2 py-1 hover:bg-gray-600 hover:text-white 
+                ${filter === "like" ? "bg-gray-600 text-white" : ""}`}
+                onClick={() => setFilter("like")}
+              >
+                추천순
+              </button>
+              <button
+                className={`border border-gray-600 px-2 py-1 hover:bg-gray-600 hover:text-white 
+                ${filter === "importance" ? "bg-gray-600 text-white" : ""}`}
+                onClick={() => setFilter("importance")}
+              >
+                중요도순
+              </button>
+            </div>
+          </div>
         </Container>
       </Layout>
     </>
